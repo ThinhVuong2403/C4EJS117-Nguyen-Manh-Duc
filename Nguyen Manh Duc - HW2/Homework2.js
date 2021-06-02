380caa2efc9782a940dcba5ca3d514827e2f5c4a
ImmenseHorse <manhducams@gmail.com>
2021-06-02 16:28:00 +0700

    add HW2

---
+++ Nguyen Manh Duc - HW2/Homework2.js
@@ -0,0 +1,19 @@
+// 1.
+
+// 1. var and const are variable declarations in JavaScript.
+
+// 2. let is block scoped, "ReferenceError" is displayed when we try to access 
+// a variable before it is declared.
+
+// 3. let and const are both block scoped, "ReferenceError" is displayed when we
+// try to access a variable before it's declared. However, let variables can be
+// re-assigned, while const variables cannot be re-assigned, only the properties
+// of the const objects can be mutated.
+
+// 4. var should not be used in any case.
+// const should be used when you want to signal a warning to yourself and others
+// looking into your code that the variable should not be re-assigned.
+// let should be used when you intend to re-assign the variable.
+
+
+// 2.
