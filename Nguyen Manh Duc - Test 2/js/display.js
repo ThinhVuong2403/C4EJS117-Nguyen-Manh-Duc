b8fa21969fa39c7a369cd89ba6d0f033ea001e08
ImmenseHorse <manhducams@gmail.com>
2021-07-09 22:27:16 +0700

    add Test 2

---
+++ Nguyen Manh Duc - Test 2/js/display.js
@@ -0,0 +1,13 @@
+let link = "http://tobitheme.net/api/characters.json";
+const thebody = document.getElementsByClassName('flex-container-main-display');
+const firstBody = thebody[0];
+displayAll(link, firstBody);
+
+const find = document.getElementById('find');
+const val = document.getElementById('search');
+
+find.addEventListener('click', () => {
+    displayName(val.value, link, firstBody);
+})
+
+displayChar(id, source, body)
---
+++ Nguyen Manh Duc - Test 2/js/searchChar.js
@@ -0,0 +1,38 @@
+function displayChar(object, body) {
+    body.insertAdjacentHTML('beforeend', `<div class="container-char"><div><b id=${object["name"]} style='font-size:30px'>${object["name"]}</b></div><div><img src=${object["image"]}></div><div>House: ${object["house"]}</div></div><br><br>`);
+}
+
+async function displayAll(url, body) {
+    const data = await fetch(url);
+    const json = await data.json();
+    const allObj = json["data"];
+    for (let i of allObj) {
+        displayChar(i, body);
+    }
+}
+
+
+async function displayName(name, source, body) {
+    const data = await fetch(source);
+    const json = await data.json();
+    const arr = json["data"];
+    body.innerHTML = '';
+    for (let i of arr) {
+        if ((i["name"].toLowerCase()).includes(name.toLowerCase())) {
+            body.insertAdjacentHTML('beforeend', `<div class="container-char"><div><b id=${object["name"]} style='font-size:30px'>${i["name"]}</b></div><div><img src=${i["image"]}></div><div>House: ${i["house"]}</div></div><br><br>`);
+        }
+    }
+}
+
+async function displayChar(id, source, body) {
+    const data = await fetch(source);
+    const json = await data.json();
+    const arr = json["data"];
+    body.innerHTML = '';
+    for (let i of arr) {
+        if (i["name"] == id) {
+            body.insertAdjacentHTML('beforeend', `<div class="container-char"><div><b style='font-size:30px'>${i["name"]}</b></div><div><img src=${i["image"]}></div><div>House: ${i["house"]}</div></div><br><br>`);
+            break;
+        }
+    }
+}
